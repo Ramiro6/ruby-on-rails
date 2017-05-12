71f2a32fd46805da0c9bd01da9ec9ea0bb29092a
Ramiro6 <fresco_esp@hotmail.com>
2017-05-12 15:48:05 +0200

    first

--- Zombie/app/assets/javascripts/components/zombie.js.jsx
+++ Zombie/app/assets/javascripts/components/zombie.js.jsx
@@ -1,24 +1,44 @@
-var User = React.createClass({
-  render() {
-    var createItem = (p) =>
-    <div>
-      <h1>{p.name} / {p.age}</h1>
-    </div>
+// var User = React.createClass({
+//   render() {
+//     var createItem = (p) =>
+//     <div>
+//       <h1>{p.name} / {p.age}</h1>
+//     </div>
+//
+//
+//   return <h4>{this.props.data.map(createItem)}</h4>
+//   }
+// });
+//
+//
+// var hi = React.createClass({
+//   render() {
+//     return <h2> Hi {this.props.name}</h2>
+//   }
+// });
+//
+// var person = React.createClass({
+//   render(){
+//     return <h2> hi {this.props.name} </h2>
+//   }
+// });
+//
+//
+// var tt = React.createClass({
+//   render(){
+//     return <h3> Second {this.props.una} </h3>
+//   }
+// });
 
 
-  return <h4>{this.props.data.map(createItem)}</h4>
-  }
-});
 
-
-var hi = React.createClass({
-  render() {
-    return <h2> Hi {this.props.name}</h2>
-  }
-});
-
-var person = React.createClass({
+var todo = React.createClass({
   render(){
-    return <h2> hi {this.props.name} </h2>
+    return <h1 style={style}> Hola a todos </h1>
   }
 });
+
+
+var style = {
+  color: 'red',
+}
