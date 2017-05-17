be229c1c19a317908465f8db0cb7b6c977145be5
Ramiro6 <fresco_esp@hotmail.com>
2017-05-17 12:08:57 +0200

    first

--- Zombie2/app/assets/javascripts/application.js
+++ Zombie2/app/assets/javascripts/application.js
@@ -14,3 +14,11 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+// $(document).ready(function(){
+//   function notify() {
+//     $(".qq").html($("#user_name").val())
+//   }
+//
+//  $(".kk").on("click", notify)
+// });
